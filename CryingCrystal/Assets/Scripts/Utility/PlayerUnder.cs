d3899ba1f104ae14b33c94936c65e01cccf5dc88
UnJay-0 <69627554+UnJay-0@users.noreply.github.com>
2022-04-02 10:24:18 +0200

    Starting implementation of Monest

---
+++ CryingCrystal/Assets/Scripts/Utility/PlayerUnder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnder : MonoBehaviour {
+    private SpriteRenderer sp;
+
+    // Start is called before the first frame update
+    void Start() {
+      sp = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+      sp.color = new Color(1, 1, 1, 0.4f);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+      sp.color = new Color(1, 1, 1, 1);
+    }
+}
